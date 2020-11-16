2891323a40195fc4b5f52b02b7720038f2d37f82
I066841 <a.kulshreshtha@sap.com>
2020-11-16 18:41:21 +0530

    How to insert documents in MongoDb

--- mongodb.js
+++ mongodb.js
@@ -5,15 +5,56 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client)=> {
-   if (error) {
-       return console.log('Unable to connect to the database')
-   }
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to the database')
+    }
 
-   const db = client.db(databaseName)
- 
-   db.collection('users').insertOne({
-       name: 'Abhijeet Kulshreshtha',
-       age: 35
-   })
+    const db = client.db(databaseName)
+
+    //    db.collection('users').insertOne({
+    //        name: 'Bob Peter',
+    //        age: 38
+    //    }, (error, result) => {
+    //        if (error) {
+    //            return console.log('Unable to insert a record')
+    //        }
+
+    //        console.log(result.ops)
+    //    })
+
+    //    db.collection('users').insertMany([
+    //        {
+    //            name: 'Max Pareira',
+    //            age: 39
+    //        },{
+    //            name: 'Noman Gonsalves',
+    //            age: 23
+    //        }
+    //    ], (error, result) => {
+    //        if (error) {
+    //            return console.log('Data could not be inserted')
+    //        }
+
+    //        console.log(result.ops)
+    //    })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Nodejs course',
+            completed: false
+        }, {
+            description: 'Docker course',
+            completed: false
+        }, {
+            description: 'Astro-physics course',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Data could not be inserted')
+        }
+
+        console.log(result.ops)
+    })
 })
