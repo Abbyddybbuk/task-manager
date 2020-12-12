06a87e8aca03e3194829b6fbb7d6f3f5455a0f09
I066841 <a.kulshreshtha@sap.com>
2020-12-13 00:27:06 +0530

    Learnt Mongoose Create/Read

---
+++ src/index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+require('./db/mongoose')
+
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        res.status(202).send(users)
+    }).catch((error) => {
+        res.status(500).send
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send('No data found')
+        }
+
+        res.status(202).send(user)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const myTask = new Task(req.body)
+
+    myTask.save().then(() => {
+        res.status(201).send(myTask)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find().then((users) => {
+        res.status(202).send(users)
+    }).catch((error) => {
+        res.status(500).send('No data found')
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        res.status(202).send(task)
+    }).catch((error) => {
+        res.status(500).send('No data found')
+    })
+})
+
+app.listen(port, () => {
+    console.log('Listening to port' + port)
+})
---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.includes('password')) {
+                throw new Error('Password should not contain password as text')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+module.exports = User
