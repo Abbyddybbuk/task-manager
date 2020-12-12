bb0eba2516ff252544e753e84bd2c3e26ba495b9
I066841 <a.kulshreshtha@sap.com>
2020-12-12 14:19:21 +0530

    Learnt Mongoose Basics

---
+++ src/db/mongoose.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const myTask = new Task({
+    description: 'Exercise',
+    completed: false
+})
+
+myTask.save().then(()=> {
+   console.log(myTask)
+}).catch((error)=> {
+   console.log(error)
+})
