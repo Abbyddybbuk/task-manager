7549475e0b94902652c77608f724618b0691141d
I066841 <a.kulshreshtha@sap.com>
2020-12-13 22:24:38 +0530

    Integrated Async/await with express REST API

--- src/index.js
+++ src/index.js
@@ -12,70 +12,76 @@ app.use(express.json())
 const port = process.env.PORT || 3000
 
 // User App Endpoints
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find().then((users) => {
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find()
         res.status(202).send(users)
-    }).catch((error) => {
-        res.status(500).send
-    })
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send('No data found')
         }
 
         res.status(202).send(user)
-    }).catch((error) => {
-        res.status(500).send()
-    })
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 
 
 // TASK App Endpoints
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const myTask = new Task(req.body)
 
-    myTask.save().then(() => {
+    try {
+        await myTask.save()
         res.status(201).send(myTask)
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find().then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find()
         res.status(202).send(tasks)
-    }).catch((error) => {
-        res.status(500).send('No data found')
-    })
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send('No data found')
         }
-
         res.status(202).send(task)
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send('No data found')
-    })
+    }
 })
 
 app.listen(port, () => {
