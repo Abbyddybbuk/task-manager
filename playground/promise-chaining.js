4af878f39eb0de269c9826f0ccd73d3ca6a65b16
I066841 <a.kulshreshtha@sap.com>
2020-12-13 19:32:28 +0530

    Async/await With Mongoose

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,11 +2,22 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5fd4f484cc4bb934e0349eb3', { age: 34 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 34 })
-}).then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
+// User.findByIdAndUpdate('5fd4f484cc4bb934e0349eb3', { age: 34 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 34 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age: age })
+    const count = await User.countDocuments({ age: age })
+    return { user: user, count: count }
+}
+
+updateAgeAndCount('5fd4f484cc4bb934e0349eb3', 32).then((resultObject)=>{
+  console.log('Updated User: ' + resultObject.user)
+  console.log('Total number of records as per supplied age: ' + resultObject.count)
 })
