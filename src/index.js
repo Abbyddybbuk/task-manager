045c2434d4077cc836bce795302c83aa9016bc49
I066841 <a.kulshreshtha@sap.com>
2020-12-18 12:25:16 +0530

    Modularization/Routing NodeJs webapp

--- src/index.js
+++ src/index.js
@@ -1,157 +1,19 @@
 const express = require('express')
 require('./db/mongoose')
-
 const User = require('./models/user')
 const Task = require('./models/task')
 
-const app = express()
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
+const app = express()
 // to get the input payload json
 app.use(express.json())
-
+app.use(userRouter)
+app.use(taskRouter)
 const port = process.env.PORT || 3000
-
-// User App Endpoints
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find()
-        res.status(202).send(users)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send('No data found')
-        }
-
-        res.status(202).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send('No data found to be deleted')
-        }
-        res.status(202).send(user)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'age', 'email', 'password']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid Payload sent' })
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.status(202).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-
-// TASK App Endpoints
-app.post('/tasks', async (req, res) => {
-    const myTask = new Task(req.body)
-
-    try {
-        await myTask.save()
-        res.status(201).send(myTask)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find()
-        res.status(202).send(tasks)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById(_id)
-        if (!task) {
-            return res.status(404).send('No data found')
-        }
-        res.status(202).send(task)
-    } catch (error) {
-        res.status(500).send('No data found')
-    }
-})
-
-app.delete('/tasks/:id', async(req, res)=> {
-    try {
-      const task = await Task.findByIdAndDelete(req.params.id)
-
-      if (!task) {
-          return res.status(404).send('No data found to be deleted')
-      }
-
-      res.status(202).send(task)
-    } catch(error) {
-       res.status(500).send('Internal Server Error')
-    }
-})
-
-app.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid Payload sent' })
-    }
-
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!task) {
-            return res.status(404).send('No data found')
-        }
-        res.status(202).send(task)
-    } catch (error) {
-        res.status(500).send('No data found')
-    }
-})
 
 app.listen(port, () => {
     console.log('Listening to port' + port)
 })
+
---
+++ src/routers/task.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const router = express.Router()
+const Task = require('../models/task')
+
+// TASK App Endpoints
+router.post('/tasks', async (req, res) => {
+    const myTask = new Task(req.body)
+
+    try {
+        await myTask.save()
+        res.status(201).send(myTask)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.status(202).send(tasks)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send('No data found')
+        }
+        res.status(202).send(task)
+    } catch (error) {
+        res.status(500).send('No data found')
+    }
+})
+
+router.delete('/tasks/:id', async(req, res)=> {
+    try {
+      const task = await Task.findByIdAndDelete(req.params.id)
+
+      if (!task) {
+          return res.status(404).send('No data found to be deleted')
+      }
+
+      res.status(202).send(task)
+    } catch(error) {
+       res.status(500).send('Internal Server Error')
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Payload sent' })
+    }
+
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send('No data found')
+        }
+        res.status(202).send(task)
+    } catch (error) {
+        res.status(500).send('No data found')
+    }
+})
+
+
+module.exports = router
