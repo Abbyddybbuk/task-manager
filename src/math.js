4fd3ca01cb300e14ca698a9ccdc117b7cc5fffda
I066841 <a.kulshreshtha@sap.com>
2021-01-06 18:14:30 +0530

    jest working with Async/await

--- src/math.js
+++ src/math.js
@@ -3,9 +3,18 @@ const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
 const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8
 
 const celsiusToFahrenheit = (temp) => (temp * 1.8) + 32
-
+const add = (a, b)=> {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if (a < 0 || b < 0)
+               reject('Numbers cannot be negative')
+            resolve(a + b)
+        }, 1000)
+    })
+}
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
 test('Tip Calculation', () => {
     const total = calculateTip(10, .3)
@@ -10,12 +10,24 @@ test('Default Tip Calculation', () => {
     expect(total).toBe(12.5)
 })
 
-test('FTOC', ()=> {
+test('FTOC', () => {
     const temp = fahrenheitToCelsius(32)
     expect(temp).toBe(0)
 })
 
-test('CTOF', ()=> {
+test('CTOF', () => {
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+test('Should add two numbers', (done) => {
+    add(4, 6).then((sum) => {
+        expect(sum).toBe(10)
+        done()
+    })
+})
+
+test('Should add two numbers via async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
