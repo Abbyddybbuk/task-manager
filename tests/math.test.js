6abed62499b63dc205eb087d49013a80944d46aa
I066841 <a.kulshreshtha@sap.com>
2021-01-06 16:13:27 +0530

    Jest Simple Examples

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
 
 test('Tip Calculation', () => {
     const total = calculateTip(10, .3)
@@ -8,4 +8,14 @@ test('Tip Calculation', () => {
 test('Default Tip Calculation', () => {
     const total = calculateTip(10)
     expect(total).toBe(12.5)
+})
+
+test('FTOC', ()=> {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('CTOF', ()=> {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
 })
